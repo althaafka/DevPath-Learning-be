8af170860cfe3e20cf035391d5531ff234d66627
althaafka <althaafka004@gmail.com>
2024-07-23 13:17:56 +0700

    feat: add class model

---
+++ models/class.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+    const Class = sequelize.define('Class', {
+      class_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      photo: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    });
+  
+    Class.associate = (models) => {
+      Class.belongsTo(models.User, {
+        foreignKey: 'user_id',
+        as: 'user'
+      });
+    };
+  
+    return Class;
+  };
+  
--- models/user.js
+++ models/user.js
@@ -1,41 +1,47 @@
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-      user_id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-      },
-      full_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      role: {
-        type: DataTypes.ENUM('student', 'teacher', 'admin'),
-        allowNull: false
-      },
-      expertise: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        defaultValue: null,
-        validate: {
-          isNullWhenStudent(value) {
-            if (this.role === 'student' && value !== null) {
-              throw new Error("Expertise must be null for students");
-            }
+  const User = sequelize.define('User', {
+    user_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    full_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.ENUM('student', 'teacher', 'admin'),
+      allowNull: false
+    },
+    expertise: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: null,
+      validate: {
+        isNullWhenStudent(value) {
+          if (this.role === 'student' && value !== null) {
+            throw new Error("Expertise must be null for students");
           }
         }
       }
+    }
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.Class, {
+      foreignKey: 'user_id',
+      as: 'classes'
     });
-  
-    return User;
   };
-  
+
+  return User;
+};
